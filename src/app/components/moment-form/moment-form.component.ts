a2bc796b8529780c75fd5233160b0125a6bfd34a
LEANDRO MOREIRA <leandrodev.oliveira@gmail.com>
2022-05-22 15:24:23 -0300

    Validacao de formulario com reactive forms

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-moment-form',
@@ -8,7 +9,35 @@ import { Component, Input, OnInit } from '@angular/core';
 export class MomentFormComponent implements OnInit {
   // o ! indica que pode ser inicializado ou não
   @Input() btnText!: string;
+
+  momentForm!: FormGroup;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  //usado para inicialização de recursos do angular
+  ngOnInit(): void {
+    this.momentForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      image: new FormControl(''),
+    });
+  }
+
+  get title() {
+    // a exclamação (!  ) no final do  this.momentForm.get('title') informa que as propriedades iraão sim existir
+    return this.momentForm.get('title')!;
+  }
+
+  get description() {
+    return this.momentForm.get('description')!;
+  }
+
+  submit(): void {
+    //se o fomr estiver invalido, não permite que o formulario seja enviado
+    if (this.momentForm.invalid) {
+      return;
+    }
+    console.log('Enviou formulario');
+  }
 }
--- src/app/services/moment.service.spec.ts
+++ src/app/services/moment.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { MommentService } from './momment.service';
+import { MommentService } from './moment.service';
 
 describe('MommentService', () => {
   let service: MommentService;
